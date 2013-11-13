cb458beb6d15ea5c49c844710a8e2c330ed5cd12
Robert Emerson <eggbert.remerson@gmail.com>
2013-11-13 00:08:24 -0500

    Initial commit

--- website.py
+++ website.py
@@ -6,6 +6,7 @@ import MySQLdb
 
 #configuration
 DATABASE =  MySQLdb.connect(host="stardock.cs.virginia.edu", user="cs4750roe2pj", passwd="NOT_MY_PASSWORD", db="cs4750roe2pj")
+recipe_categories = ['home', 'appetizers', 'main dishes', 'side dishes', 'desserts', 'all']
 
 # create our little application :)
 app = Flask(__name__)
@@ -16,18 +17,23 @@ app.config.from_object(__name__)
 @app.route('/home/')
 def index():
     navigation = buildNavigation()
-    title = 'Home'
+    title = 'home'
     subtitle = ""
     return render_template('index.html', navigation=navigation, title=title, subtitle=subtitle)
 
-@app.route('/recipe/<name>')
+@app.route('/recipes/<name>')
 def recipe():
     pass
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('index.html'), 404 #TODO write a creative 404 page
+
 def buildNavigation():
-    recipe_categories = ['home', 'appetizers', 'main dishes', 'side dishes', 'desserts']
+
     recipe_pages = []
     for category in recipe_categories:
+
         recipe_pages.append(NavigationItem(category))
     return recipe_pages
 
@@ -36,7 +42,10 @@ class NavigationItem:
     caption = ''
 
     def __init__(self, captionIn):
-        self.href = '../' + str.replace(captionIn, " ", "_")
+        if captionIn == 'home':
+            self.href = '../'
+        else:
+            self.href = '../recipes/' + str.replace(captionIn, " ", "_")
         self.caption = captionIn
 
 if __name__ == '__main__':
